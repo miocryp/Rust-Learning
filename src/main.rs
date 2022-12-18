b66ca4b2d736d7b53116ae0a9d33bede135b473b
miocryp <mio.cryp@gmaill.com>
2022-12-19 00:06:48 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@
 //mod functions;
 //mod pointer_ref;
 //mod structs;
-mod enums;
+// mod enums;
+mod cli;
 fn main() {
     //operators::run();
     //helloworld::run();
@@ -25,7 +26,8 @@ fn main() {
     //functions::run();
     //pointer_ref::run();
     //structs::run();
-    enums::run();
+    // enums::run();
+    cli::run();
 
 
 }
