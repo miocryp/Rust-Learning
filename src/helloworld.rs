8942ed78e9012a90aca3f5523ae71c91c1a831f0
miocryp <mio.cryp@gmaill.com>
2022-12-18 01:05:45 +0100

    hello world

--- src/helloworld.rs
+++ src/helloworld.rs
@@ -3,4 +3,14 @@ pub fn run() {
 
     // Print text to the console
     println!("Hello World!");
+    println!("{}", 10);
+    // Basic formatting
+    println!("{} is my friend, from {}", "Brandon", "USA");
+    // Positional Argument
+    println!("{0} is from {1} and {0} likes to {2}", "Brandon", "USA", "Code");
+    // Named Argument
+    println!("{name} likes to play {activity}", name = "Bradon", activity = "Baseball");
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:0}", 10, 10, 10);
 }
