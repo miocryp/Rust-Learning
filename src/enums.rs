b54e916dee662143ebca8652623b86ebbfc3093e
miocryp <mio.cryp@gmaill.com>
2022-12-19 00:06:33 +0100

    enum

--- src/enums.rs
+++ src/enums.rs
@@ -22,7 +22,7 @@ pub fn run() {
     let avatar1 = Movement::Left;
     let avatar2 = Movement::Up;
     let avatar3 = Movement::Right;
-    let avatar4 = Movement::Down;
+     let avatar4 = Movement::Down;
 
     move_avatar(avatar1);
     move_avatar(avatar2);
